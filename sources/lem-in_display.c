25df7608642b950f026dfaca204fd50e3e42544f
Mathis Pandelé <mapandel@student.42.fr>
2017-08-31 00:31:44 +0200

    lem-in_display.c 1.1 : Debug tool

--- sources/lem-in_display.c
+++ sources/lem-in_display.c
@@ -6,15 +6,30 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/15 11:43:54 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/15 11:46:24 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/28 19:03:24 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
 
+void		lem_in_display_path(t_lem_in_path *li_p)
+{
+	t_lem_in_path	*tmp;
+
+	tmp = lem_in_path_start(li_p);
+	while (tmp)
+	{
+		ft_putnbr(tmp->room);
+		tmp = tmp->next;
+		if (tmp)
+			ft_putchar('-');
+	}
+	ft_putendl("");
+}
+
 void		lem_in_display_error(t_lem_in *li)
 {
 	del_t_lem_in(li);
-	ft_putstr("\033[31mERROR\033[0m");
+	ft_putendl("\n\033[31mERROR\033[0m");
 	exit (-1);
 }
