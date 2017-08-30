9f16d923008bb1e59bbf5d315a34849d039b0e4a
Mathis Pandelé <mapandel@student.42.fr>
2017-08-31 00:31:00 +0200

    lem-in_checker.c 1.1 : Searching paths

--- sources/lem-in_checker.c
+++ sources/lem-in_checker.c
@@ -6,15 +6,20 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 22:04:08 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/23 23:48:36 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/26 21:32:02 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
 
-void	lem_in_search_paths(t_lem_in *li)
+int		lem_in_is_banned(t_lem_in *li, t_lem_in_room *room)
 {
-	(void)li;
+	if (ft_tabfindpos(li->used_rooms, lem_in_find_pos(li, room)))
+		return (1);
+	else if (li->used_rooms->len > 0
+		&& li->used_rooms->tab[0] == lem_in_find_pos(li, room))
+		return (1);
+	return (0);
 }
 
 int		lem_in_checker_doubles(t_lem_in *li, char *name, int x, int y)
