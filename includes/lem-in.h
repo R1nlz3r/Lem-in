f2aac6967c8abf13d2c0277ab426f552acda5181
Mathis Pandelé <mapandel@student.42.fr>
2017-08-31 00:27:55 +0200

    lem-in.h 1.1 : Searching paths

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/12 17:20:29 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/23 23:48:29 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/28 19:09:48 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define LEM_IN_H
 
 #include "libft.h"
+
+typedef struct				s_lem_in_path
+{
+	int						room;
+	int						len;
+	struct s_lem_in_path	*next;
+	struct s_lem_in_path	*previous;
+}							t_lem_in_path;
 
 typedef struct				s_lem_in_room
 {
@@ -23,7 +31,7 @@ typedef struct				s_lem_in_room
 	int						start;
 	int						end;
 	int						nb_nexts;
-	char					pad_0[4];
+	char					pad_1[4];
 	struct s_lem_in_room	**next;
 }							t_lem_in_room;
 
@@ -35,10 +43,17 @@ typedef struct				s_lem_in
 	int						p_nb_ant;
 	int						p_rooms;
 	int						p_pipes;
-	char					pad_1[4];
 	int						nb_ant;
 	int						nb_rooms;
+	int						nb_paths;
+	int						start_pos;
+	int						end_pos;
+	int						travel_dir;
+	int						cur_path;
+	t_tab					*used_rooms;
 	t_lem_in_room			**anthill;
+	t_lem_in_path			**paths;
+	t_lem_in_path			**paths_start;
 }							t_lem_in;
 
 t_lem_in					*init_t_lem_in(t_lem_in *li);
@@ -48,11 +63,22 @@ void						del_t_lem_in_room(t_lem_in_room *li_r);
 void						lem_in_add_room(t_lem_in *li);
 int							lem_in_link_rooms(t_lem_in_room *r1,
 	t_lem_in_room *r2);
+t_lem_in_path				*init_t_lem_in_path(t_lem_in *li,
+	t_lem_in_path *li_p, t_lem_in_path *previous, t_lem_in_room *room);
+void						del_t_lem_in_path_chain(t_lem_in_path *li_p);
+void						del_t_lem_in_path(t_lem_in_path *li_p);
+t_lem_in_path				*lem_in_path_start(t_lem_in_path *li_p);
+t_tab						*add_banned_room(t_tab *used_rooms, int banned_pos);
+void						del_banned_room(t_tab *used_rooms,
+	int unbanned_pos);
+int							lem_in_find_pos(t_lem_in *li, t_lem_in_room *room);
 void						lem_in_parsing(t_lem_in *li);
 int							lem_in_checker_doubles(t_lem_in *li, char *name,
 	int x, int y);
+int							lem_in_is_banned(t_lem_in *li, t_lem_in_room *room);
 void						lem_in_search_paths(t_lem_in *li);
 void						lem_in_display_error(t_lem_in *li)
 	__attribute__ ((noreturn));
+void						lem_in_display_path(t_lem_in_path *li_p);
 
 #endif
