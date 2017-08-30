d1444097ed4aed0812e39acd79ffcabd7022e469
Mathis Pandelé <mapandel@student.42.fr>
2017-08-31 00:30:17 +0200

    touch_t_lem_in_2.c 1.0 : Implementation

---
+++ sources/touch_t_lem_in_2.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   touch_t_lem_in_2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/24 12:50:06 by mapandel          #+#    #+#             */
+/*   Updated: 2017/08/28 19:09:45 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem-in.h"
+
+void			del_banned_room(t_tab *used_rooms, int unbanned_pos)
+{
+	size_t		i;
+
+	i = 0;
+	while (i < used_rooms->len && used_rooms->tab[i] != unbanned_pos)
+		++i;
+	used_rooms->tab[i] = -1;
+}
+
+t_tab			*add_banned_room(t_tab *used_rooms, int banned_pos)
+{
+	size_t		i;
+
+	i = 0;
+	while (i < used_rooms->len && used_rooms->tab[i] != -1)
+		++i;
+	if (i == used_rooms->len)
+		used_rooms = ft_tabadd_leakless(used_rooms, banned_pos);
+	else
+		used_rooms->tab[i] = banned_pos;
+	return (used_rooms);
+}
+
+void			del_t_lem_in_path(t_lem_in_path *li_p)
+{
+	t_lem_in_path	*tmp;
+
+	while (li_p)
+	{
+		tmp = li_p->next;
+		ft_memdel((void**)&li_p);
+		li_p = tmp;
+	}
+}
+
+void			del_t_lem_in_path_chain(t_lem_in_path *li_p)
+{
+	t_lem_in_path	*tmp;
+
+	if ((tmp = li_p->previous))
+		li_p->previous->next = NULL;
+	ft_memdel((void**)&li_p);
+	li_p = tmp;
+}
+
+t_lem_in_path	*init_t_lem_in_path(t_lem_in *li, t_lem_in_path *li_p,
+	t_lem_in_path *previous, t_lem_in_room *room)
+{
+	if (!(li_p = ft_memalloc(sizeof(t_lem_in_path))))
+		exit (-1);
+	li_p->room = lem_in_find_pos(li, room);
+	li_p->next = NULL;
+	if (!previous)
+		li_p->len = 0;
+	else
+		li_p->len = previous->len + 1;
+	li_p->previous = NULL;
+	return (li_p);
+}
