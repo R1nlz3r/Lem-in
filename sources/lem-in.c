4e4cd84105e289a6632e2054050dd8099bdf1456
Mathis PANDELE <mapandel@e2r3p20.42.fr>
2017-08-31 09:38:15 +0200

    lem-in.c 1.1 : Searching paths

--- sources/lem-in.c
+++ sources/lem-in.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/12 19:58:13 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/23 23:49:55 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/31 07:33:49 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int			main(void)
 	li = NULL;
 	li = init_t_lem_in(li);
 	lem_in_parsing(li);
+	lem_in_prepare_search_paths(li);
 	lem_in_search_paths(li);
-	del_t_lem_in(li);
-	return (0);
 }
