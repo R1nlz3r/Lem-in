39c963cecad4beeca3195a2291c0c0478baca41f
Mathis PANDELE <mapandel@e2r3p20.42.fr>
2017-08-31 09:40:05 +0200

    lem-in_display.c 1.2 : Add debug tool

--- sources/lem-in_display.c
+++ sources/lem-in_display.c
@@ -6,11 +6,26 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/15 11:43:54 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/28 19:03:24 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/08/31 07:57:56 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
+
+void		lem_in_display_banned_room(t_lem_in *li)
+{
+	size_t		i;
+
+	i = 0;
+	while (i < li->used_rooms->len)
+	{
+		ft_putnbr(li->used_rooms->tab[i]);
+		++i;
+		if (i != li->used_rooms->len)
+			ft_putchar('-');
+	}
+	ft_putendl("");
+}
 
 void		lem_in_display_path(t_lem_in_path *li_p)
 {
