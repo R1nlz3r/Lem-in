91d1f78f29e4747c291b9997621c090723e3534a
Mathis Pandelé <mapandel@student.42.fr>
2017-09-06 12:28:08 +0200

    lem-in_checker.c 1.2 : Moving ants

--- sources/lem-in_checker.c
+++ sources/lem-in_checker.c
@@ -6,11 +6,33 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/20 22:04:08 by mapandel          #+#    #+#             */
-/*   Updated: 2017/08/26 21:32:02 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/06 03:26:21 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
+
+int		lem_in_checker_ants(t_lem_in *li)
+{
+	t_lem_in_path	*tmp;
+	int				i;
+
+	i = 0;
+	if (li->nb_ant != li->nb_ant_backup)
+		return (1);
+	while (i < li->nb_paths)
+	{
+		tmp = lem_in_path_start(li->paths[i]);
+		while (tmp)
+		{
+			if (tmp->ant)
+				return (1);
+			tmp = tmp->next;
+		}
+		++i;
+	}
+	return (0);
+}
 
 int		lem_in_is_banned(t_lem_in *li, t_lem_in_room *room)
 {
