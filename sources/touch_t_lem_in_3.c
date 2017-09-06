7c9ec439273c4691cfd873888e3cfe766b91dfa5
Mathis Pandelé <mapandel@student.42.fr>
2017-09-06 12:27:28 +0200

    touch_t_lem_in_3.c 1.2 : Moving ants

--- sources/touch_t_lem_in_3.c
+++ sources/touch_t_lem_in_3.c
@@ -6,11 +6,37 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/26 22:46:37 by mapandel          #+#    #+#             */
-/*   Updated: 2017/09/02 06:45:33 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/06 03:10:45 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
+
+void				lem_in_sort_rooms(t_lem_in *li)
+{
+	t_lem_in_path	*end;
+	int				tmp;
+	int				i;
+
+	i = 0;
+	if (!li->travel_dir)
+		return ;
+	while (i < li->nb_paths)
+	{
+		end = lem_in_path_end(li->paths[i]);
+		li->paths[i] = lem_in_path_start(li->paths[i]);
+		while (li->paths[i] != end && li->paths[i]->previous != end)
+		{
+			tmp = li->paths[i]->room;
+			li->paths[i]->room = end->room;
+			end->room = tmp;
+			end = end->previous;
+			li->paths[i] = li->paths[i]->next;
+		}
+		++i;
+		li->paths[i] = lem_in_path_start(li->paths[i]);
+	}
+}
 
 void				lem_in_sort_paths(t_lem_in *li)
 {
@@ -19,6 +45,9 @@ void				lem_in_sort_paths(t_lem_in *li)
 	int				i;
 	int				j;
 
+	i = 0;
+	while (i < li->nb_paths && (li->paths[i] = lem_in_path_end(li->paths[i])))
+		++i;
 	i = 0;
 	while (i < li->nb_paths - 1)
 	{
