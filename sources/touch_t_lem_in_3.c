cbcbdf314062d9df7e6b549a1f54895ceeef58c4
Mathis PANDELE <mapandel@e2r5p21.42.fr>
2017-09-10 15:58:16 +0200

    touch_t_lem_in_3.c 1.3 : Norme

--- sources/touch_t_lem_in_3.c
+++ sources/touch_t_lem_in_3.c
@@ -6,85 +6,11 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/26 22:46:37 by mapandel          #+#    #+#             */
-/*   Updated: 2017/09/06 03:10:45 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/10 13:44:52 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "lem-in.h"
-
-void				lem_in_sort_rooms(t_lem_in *li)
-{
-	t_lem_in_path	*end;
-	int				tmp;
-	int				i;
-
-	i = 0;
-	if (!li->travel_dir)
-		return ;
-	while (i < li->nb_paths)
-	{
-		end = lem_in_path_end(li->paths[i]);
-		li->paths[i] = lem_in_path_start(li->paths[i]);
-		while (li->paths[i] != end && li->paths[i]->previous != end)
-		{
-			tmp = li->paths[i]->room;
-			li->paths[i]->room = end->room;
-			end->room = tmp;
-			end = end->previous;
-			li->paths[i] = li->paths[i]->next;
-		}
-		++i;
-		li->paths[i] = lem_in_path_start(li->paths[i]);
-	}
-}
-
-void				lem_in_sort_paths(t_lem_in *li)
-{
-	t_lem_in_path	*tmp;
-	int				tmppos;
-	int				i;
-	int				j;
-
-	i = 0;
-	while (i < li->nb_paths && (li->paths[i] = lem_in_path_end(li->paths[i])))
-		++i;
-	i = 0;
-	while (i < li->nb_paths - 1)
-	{
-		tmppos = i;
-		j = i + 1;
-		while (j < li->nb_paths)
-		{
-			if (li->paths[tmppos]->len > li->paths[j]->len)
-				tmppos = j;
-			++j;
-		}
-		tmp = li->paths[i];
-		li->paths[i] = li->paths[tmppos];
-		li->paths[tmppos] = tmp;
-		++i;
-	}
-}
-
-t_lem_in_path		*lem_in_path_end(t_lem_in_path *li_p)
-{
-	t_lem_in_path	*tmp;
-
-	tmp = li_p;
-	while (tmp && tmp->next)
-		tmp = tmp->next;
-	return (tmp);
-}
-
-t_lem_in_path		*lem_in_path_start(t_lem_in_path *li_p)
-{
-	t_lem_in_path	*tmp;
-
-	tmp = li_p;
-	while (tmp && tmp->previous)
-		tmp = tmp->previous;
-	return (tmp);
-}
+#include "lem_in.h"
 
 int					lem_in_find_pos(t_lem_in *li, t_lem_in_room *room)
 {
@@ -108,4 +34,28 @@ void				lem_in_prepare_search_paths(t_lem_in *li)
 		li->travel_dir = 1;
 	else
 		li->used_rooms = add_banned_room(li->used_rooms, li->start_pos);
+}
+
+void				del_banned_room(t_tab *used_rooms, int unbanned_pos)
+{
+	size_t		i;
+
+	i = 0;
+	while (i < used_rooms->len && used_rooms->tab[i] != unbanned_pos)
+		++i;
+	used_rooms->tab[i] = -1;
+}
+
+t_tab				*add_banned_room(t_tab *used_rooms, int banned_pos)
+{
+	size_t		i;
+
+	i = 0;
+	while (i < used_rooms->len && used_rooms->tab[i] != -1)
+		++i;
+	if (i == used_rooms->len)
+		used_rooms = ft_tabadd_leakless(used_rooms, banned_pos);
+	else
+		used_rooms->tab[i] = banned_pos;
+	return (used_rooms);
 }
