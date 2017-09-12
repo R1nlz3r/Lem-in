fef05a92f0eaaa54ffa1f2cd86f1868e3cc8e427
Mathis Pandelé <mapandel@student.42.fr>
2017-09-12 06:39:47 +0200

    lem_in_pathfinder.c 1.5 : Norme

--- sources/lem_in_pathfinder.c
+++ sources/lem_in_pathfinder.c
@@ -6,13 +6,39 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/26 14:18:59 by mapandel          #+#    #+#             */
-/*   Updated: 2017/09/10 15:39:33 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/12 06:33:32 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-static void		lem_in_search_paths_backtrack(t_lem_in *li, t_lem_in_path *li_p,
+static int		lem_in_search_paths_backtrack2(t_lem_in *li,
+	t_lem_in_path *li_p, t_lem_in_room *cur, int *cur_next)
+{
+	while ((*cur_next < cur->nb_nexts
+		&& lem_in_is_banned(li, cur->next[*cur_next]))
+		|| (((li_p->room == li->start_pos || li_p->room == li->end_pos)
+		&& (lem_in_find_pos(li, cur->next[*cur_next]) == li->start_pos
+		|| lem_in_find_pos(li, cur->next[*cur_next]) == li->end_pos))
+		&& (li->b_littlepath = 1)))
+		++(*cur_next);
+	if (*cur_next == cur->nb_nexts)
+		return (0);
+	li_p->next = init_t_lem_in_path(li, li_p->next,
+		li_p, cur->next[*cur_next]);
+	li_p->next->previous = li_p;
+	li_p = li_p->next;
+	if ((!li->travel_dir && lem_in_find_pos(li, cur->next[*cur_next])
+		!= li->end_pos) || (li->travel_dir && lem_in_find_pos(li,
+		cur->next[*cur_next]) != li->start_pos))
+		li->used_rooms = add_banned_room(li->used_rooms, lem_in_find_pos(li,
+		cur->next[*cur_next]));
+	lem_in_search_paths_backtrack(li, li_p, cur->next[*cur_next]);
+	++(*cur_next);
+	return (1);
+}
+
+void			lem_in_search_paths_backtrack(t_lem_in *li, t_lem_in_path *li_p,
 	t_lem_in_room *cur)
 {
 	int		cur_next;
@@ -24,29 +50,9 @@ static void		lem_in_search_paths_backtrack(t_lem_in *li, t_lem_in_path *li_p,
 		++li->nb_paths;
 		lem_in_search_paths(li);
 	}
-	while (cur_next < cur->nb_nexts)
-	{
-		while ((cur_next < cur->nb_nexts
-			&& lem_in_is_banned(li, cur->next[cur_next]))
-			|| (((li_p->room == li->start_pos || li_p->room == li->end_pos)
-			&& (lem_in_find_pos(li, cur->next[cur_next]) == li->start_pos
-			|| lem_in_find_pos(li, cur->next[cur_next]) == li->end_pos))
-			&& (li->b_littlepath = 1)))
-			++cur_next;
-		if (cur_next == cur->nb_nexts)
-			break ;
-		li_p->next = init_t_lem_in_path(li, li_p->next,
-			li_p, cur->next[cur_next]);
-		li_p->next->previous = li_p;
-		li_p = li_p->next;
-		if ((!li->travel_dir && lem_in_find_pos(li, cur->next[cur_next])
-			!= li->end_pos) || (li->travel_dir && lem_in_find_pos(li,
-			cur->next[cur_next]) != li->start_pos))
-			li->used_rooms = add_banned_room(li->used_rooms, lem_in_find_pos(li,
-			cur->next[cur_next]));
-		lem_in_search_paths_backtrack(li, li_p, cur->next[cur_next]);
-		++cur_next;
-	}
+	while (cur_next < cur->nb_nexts &&
+		lem_in_search_paths_backtrack2(li, li_p, cur, &cur_next))
+		;
 	del_banned_room(li->used_rooms, lem_in_find_pos(li, cur));
 }
 
