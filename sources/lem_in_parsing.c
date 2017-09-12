50ed5e74d53e3835a5f79092e1b76fc3f47274c0
Mathis Pandelé <mapandel@student.42.fr>
2017-09-12 06:39:00 +0200

    lem_in_parsing.c 1.5 : Norme

--- sources/lem_in_parsing.c
+++ sources/lem_in_parsing.c
@@ -6,40 +6,26 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/14 22:50:03 by mapandel          #+#    #+#             */
-/*   Updated: 2017/09/10 15:44:50 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/09/12 06:20:45 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "get_next_line.h"
 
-static void		lem_in_parsing_pipes(t_lem_in *li)
+static void		lem_in_parsing_rooms2(t_lem_in *li, char **split)
 {
-	int		i;
-	int		j;
-
-	if ((i = 0) || !li->nb_rooms || li->nb_rooms == 1 || li->start_boo != 2
-		|| li->end_boo != 2 || ft_strclen(li->line, ' ') != ft_strlen(li->line))
-		li->p_pipes = 0;
-	while (i < li->nb_rooms)
-	{
-		if (!(j = 0) && ft_strinc(li->anthill[i]->name, li->line)
-			&& li->line[ft_strlen(li->anthill[i]->name)] == '-')
-		{
-			while (j < li->nb_rooms)
-			{
-				if (i != j && ft_strinc(li->anthill[j]->name,
-					&li->line[ft_strlen(li->anthill[i]->name) + 1])
-					&& !li->line[ft_strlen(li->anthill[i]->name) + 1
-					+ ft_strlen(li->anthill[j]->name)]
-					&& !lem_in_link_rooms(li->anthill[i], li->anthill[j]))
-					return ;
-				++j;
-			}
-		}
-		++i;
-	}
-	li->p_pipes = 0;
+	lem_in_add_room(li);
+	li->anthill[li->nb_rooms]->name = ft_strdup(split[0]);
+	li->anthill[li->nb_rooms]->x_coor = ft_atoi(split[1]);
+	li->anthill[li->nb_rooms]->y_coor = ft_atoi(split[2]);
+	if (li->start_boo == 1 && (li->start_boo = 2)
+		&& (li->anthill[li->nb_rooms]->start = 1))
+		li->start_pos = li->nb_rooms;
+	else if (li->end_boo == 1 && (li->end_boo = 2)
+		&& (li->anthill[li->nb_rooms]->end = 1))
+		li->end_pos = li->nb_rooms;
+	++li->nb_rooms;
 }
 
 static void		lem_in_parsing_rooms(t_lem_in *li)
@@ -57,19 +43,7 @@ static void		lem_in_parsing_rooms(t_lem_in *li)
 			ft_atoi(split[2])))
 			li->p_rooms = 0;
 		else
-		{
-			lem_in_add_room(li);
-			li->anthill[li->nb_rooms]->name = ft_strdup(split[0]);
-			li->anthill[li->nb_rooms]->x_coor = ft_atoi(split[1]);
-			li->anthill[li->nb_rooms]->y_coor = ft_atoi(split[2]);
-			if (li->start_boo == 1 && (li->start_boo = 2)
-				&& (li->anthill[li->nb_rooms]->start = 1))
-				li->start_pos = li->nb_rooms;
-			else if (li->end_boo == 1 && (li->end_boo = 2)
-				&& (li->anthill[li->nb_rooms]->end = 1))
-				li->end_pos = li->nb_rooms;
-			++li->nb_rooms;
-		}
+			lem_in_parsing_rooms2(li, split);
 	}
 	else
 		li->p_rooms = 0;
